9f851a5ee3646debe98ae7bbba73bd4b73ca67e3
Maxime Oudin <oudinmaxime@gmail.com>
2023-10-19 14:14:11 +0200

    comparison test ok

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+let score = 0
+const motApplication = "Bonjour" // Essayez de mettre un autre mot ici !
+let motUtilisateur = prompt("Entrez le mot : " + motApplication)
+
+if (motUtilisateur === motApplication) {
+    console.log("Bravo !")
+    score += 1
+} else {
+    console.log("Vous avez fait une erreur de frappe.")
+}
+
+console.log(score);
