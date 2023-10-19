01c2340e45ad015556c3413621d20e007461488c
Maxime Oudin <oudinmaxime@gmail.com>
2023-10-19 14:41:26 +0200

    refacto exercise with for loop

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
 let listeMots = ["Cachalot", "Pétunia", "Serviette"]
 let score = 0
 
-let motUtilisateur = prompt("Entrez le mot : " + listeMots[0])
-
-if (motUtilisateur === listeMots[0]) {
-    console.log("Bravo !")
-    score += 1
-    console.log("Votre score est de " + score);
-} else {
-    console.log("Vous avez fait une erreur de frappe.")
+for (let compteur = 0; compteur < (listeMots.length); compteur ++){
+  motUtilisateur = prompt("Entrez le mot : " + listeMots[compteur])
+  if (motUtilisateur === listeMots[compteur]) {
+    score ++
+    console.log("Bravo ! Votre score est de " + score)
+  }
 }
-
-motUtilisateur = prompt("Entrez le mot : " + listeMots[1])
-
-if (motUtilisateur === listeMots[1]) {
-  console.log("Bravo !")
-  score += 1
-  console.log("Votre score est de " + score);
-} else {
-  console.log("Vous avez fait une erreur de frappe.")
-}
-
-motUtilisateur = prompt("Entrez le mot : " + listeMots[2])
-
-if (motUtilisateur === listeMots[2]) {
-  console.log("Bravo !")
-  score += 1
-  console.log("Votre score est de " + score);
-} else {
-  console.log("Vous avez fait une erreur de frappe.")
-}
-
-console.log("Votre score est de " + score);
