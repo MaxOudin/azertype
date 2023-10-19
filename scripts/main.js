3c803990ef722ce65c9effb6087f0f8c7cb10368
Maxime Oudin <oudinmaxime@gmail.com>
2023-10-19 22:02:09 +0200

    event listener on radio and words list changing according

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,9 @@
 // Débuter le jeu
 lancerJeu()
 
-let inputEcriture = document.getElementById("inputEcriture")
-let btnValiderMot = document.getElementById("btnValiderMot")
-let zoneProposition = document.querySelector(".zoneProposition")
-let spanScore = document.querySelector(".zoneScore span")
-
-let listeBtnRadio = document.querySelectorAll(".optionSource input")
-console.log(listeBtnRadio);
+// let inputEcriture = document.getElementById("inputEcriture")
+// let btnValiderMot = document.getElementById("btnValiderMot")
+// let zoneProposition = document.querySelector(".zoneProposition")
+// let spanScore = document.querySelector(".zoneScore span")
+// let listeBtnRadio = document.querySelectorAll(".optionSource input")
+// console.log(listeBtnRadio);
